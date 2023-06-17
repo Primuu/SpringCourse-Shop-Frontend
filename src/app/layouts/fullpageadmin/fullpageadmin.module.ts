9ffc828133ebb75816960c7749b57fc922486f5b
Adam <trentowski.adam@gmail.com>
2023-06-17 13:59:22 +0200

    Restyled Admin Panel

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FullpageadminComponent } from './fullpageadmin.component';
 import { AdminComponent } from 'src/app/modules/admin/admin.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ]
 })
 export class FullpageadminModule { }
